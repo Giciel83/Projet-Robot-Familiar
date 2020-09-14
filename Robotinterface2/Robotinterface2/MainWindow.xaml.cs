815289bac4d913747f6214b656ed9c1a8a799154
Thomas Familiar <61021596+Thomasfamiliar@users.noreply.github.com>
2020-09-14 03:55:29 +0200

    Amélio. envoi message
    Ajout de l'évenement KeyUp dans la textBox de l'emission afin de pouvoir envoyer des message en appuyant sur la touche entrée.

--- Robotinterface2/Robotinterface2/MainWindow.xaml.cs
+++ Robotinterface2/Robotinterface2/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,14 +25,25 @@ namespace Robotinterface2
         {
             InitializeComponent();
         }
-        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+
+        void Sendmessage()
         {
             String prevtexteR = textBoxReception.Text;
             String texteE = textBoxEmission.Text;
             textBoxReception.Text = prevtexteR + "Reçu : " + texteE + "\n";
             textBoxEmission.Text = "";
-
         }
-        
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            Sendmessage();
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                Sendmessage();
+            }
+        }
     }
 }
